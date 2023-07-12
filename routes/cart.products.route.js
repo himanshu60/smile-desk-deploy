5a1e504a207e6b77b0b25190a8c37b4a4b2c7a58
himanshu raghuvanshi <baliyanh10@gmail.com>
2023-07-12 19:00:48 +0530

    flipkart-backend

--- routes/cart.products.route.js
+++ routes/cart.products.route.js
@@ -1,39 +1,54 @@
-
 const express = require("express");
 const { CartProductModel } = require("../models/cart.product.model");
 const cartProductsRouter = express.Router();
 
-
-cartProductsRouter.get("/", async (req, res) => {
-    const uid = req.body.user_id
-    try {
-        const data = await CartProductModel.find({ user_id: uid });
-        res.send(data);
-    } catch (error) {
-        console.log(error);
-        res.send(error);
+cartProductsRouter.get("/:id", async (req, res) => {
+  // const uid = req.body.user_id
+  const id = req.params.id;
+  try {
+    if (id) {
+      const data = await CartProductModel.find({ user_id: id });
+      res.send(data);
+    } else {
+      res.send([]);
     }
-})
-
-cartProductsRouter.post('/', async (req, res) => {
-    try {
-        const cartproduct = new CartProductModel({ ...req.body, quantity: 1 });
-        await cartproduct.save();
-        res.send("Product has been added to the Cart");
-    } catch (error) {
-        console.log(error);
-        res.send(error);
-    }
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
 });
 
-cartProductsRouter.delete('/:id', async (req, res) => {
-    try {
-        await CartProductModel.findByIdAndDelete({ _id: req.params.id });
-        res.send("Product removed from the cart");
-    } catch (error) {
-        console.log(error);
-        res.send(error);
-    }
-})
+cartProductsRouter.post("/", async (req, res) => {
+  try {
+    const cartproduct = new CartProductModel(req.body);
+    await cartproduct.save();
+    res.send("Product has been added to the Cart");
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
 
-module.exports = { cartProductsRouter }
+cartProductsRouter.delete("/:id", async (req, res) => {
+  try {
+    await CartProductModel.findByIdAndDelete({ _id: req.params.id });
+    res.send("Product removed from the cart");
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
+
+cartProductsRouter.post("/qtn/:id", async (req, res) => {
+  try {
+    const a = req.params.id;
+    await CartProductModel.findByIdAndUpdate({ _id: a }, req.body);
+    console.log("done");
+    res.send("quantity has been updated");
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
+
+module.exports = { cartProductsRouter };
--- routes/user.route.js
+++ routes/user.route.js
@@ -5,64 +5,70 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 userRouter.get("/", async (req, res) => {
-    try {
-        const data = await UserModel.find();
-        res.json(data);
-    } catch (error) {
-        console.log(error);
-        res.json({ msg: error });
-    }
-})
-
+  try {
+    const data = await UserModel.find();
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+    res.json({ msg: error });
+  }
+});
 
 userRouter.post("/register", async (req, res) => {
-    const { name, email, phone, password } = req.body;
-    try {
-        bcrypt.hash(password, 5, async (err, hashed_pass) => {
-            if (err) {
-                res.send("Something went wrong");
-                console.log(err);
-            } else {
-                const user = new UserModel({ name, email, phone, password: hashed_pass });
-                await user.save();
-                res.json("User has been registered");
-            }
+  const { name, email, phone, password } = req.body;
+  try {
+    bcrypt.hash(password, 5, async (err, hashed_pass) => {
+      if (err) {
+        res.send("Something went wrong");
+        console.log(err);
+      } else {
+        const user = new UserModel({
+          name,
+          email,
+          phone,
+          password: hashed_pass,
         });
-    } catch (error) {
-        console.log(error);
-        res.json({ msg: error });
-    }
+        await user.save();
+        res.json("User has been registered");
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({ msg: error });
+  }
 });
-
 
 userRouter.post("/login", async (req, res) => {
-    const { email, password } = req.body;
-    const user = await UserModel.findOne({ email });
-    try {
-        if (user) {
-            bcrypt.compare(password, user.password, (err, result) => {
-                if (err) {
-                    res.status(500).send("Something went wrong");
-                    console.log(err);
-                } else {
-                    if (result) {
-                        const token = jwt.sign({ _id: user._id }, process.env.key);
-                        res.json({ msg: "Login Successfull", token });
-                    } else {
-                        res.status(401).json("Wrong Credentials");
-                    }
-                }
-            })
+  const { email, password } = req.body;
+  const user = await UserModel.findOne({ email });
+  try {
+    if (user) {
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (err) {
+          res.status(500).send("Something went wrong");
+          console.log(err);
         } else {
-            res.status(404).json("User Not found");
+          if (result) {
+            const token = jwt.sign({ _id: user._id }, process.env.key);
+            console.log(token);
+            res.json({
+              msg: "Login Successfull",
+              token,
+              name: user.name,
+              userid: user._id,
+            });
+          } else {
+            res.status(401).json("Wrong Credentials");
+          }
         }
-    } catch (error) {
-        console.log(error);
-        res.json({ msg: error });
+      });
+    } else {
+      res.status(404).json("User Not found");
     }
+  } catch (error) {
+    console.log(error);
+    res.json({ msg: error });
+  }
 });
-
-
-
 
 module.exports = { userRouter };
